c6056c42d5cf2a0577e6275dc970947615d87ec7
AhMed SaLah <appsxapp@gmail.com>
2021-04-29 04:45:55 +0200

    Created Loading Indicator

--- components/Decks.js
+++ components/Decks.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react"
-import { FlatList, TouchableOpacity, View } from "react-native"
+import {
+	ActivityIndicator,
+	FlatList,
+	StyleSheet,
+	TouchableOpacity,
+	Text,
+	View,
+} from "react-native"
 import { getDecksAsync } from "../utils/helpers"
 import DeckCard from "./DeckCard"
 
 const Decks = ({ navigation }) => {
 	const [ decks, setDecks ] = useState([])
+	const [ isReady, setIsReady ] = useState(false)
 
 	useEffect(() => {
 		getDecksAsync()
 			.then((decks) =>
 				setDecks((ds) => ds.concat(Object.values(!decks ? {} : decks))),
 			)
+			.then(() => setIsReady((isReady) => !isReady))
 			.catch((error) =>
 				console.warn("Error setting decks into state: ", error),
 			)
@@ -24,9 +33,19 @@ const Decks = ({ navigation }) => {
 		</TouchableOpacity>
 	)
 
-	return (
+	return !isReady ? (
+		<View style={styles.loadingIndicator}>
+			<ActivityIndicator size="large" color="#00ff00" />
+		</View>
+	) : (
 		<View style={{ flex: 1 }}>
-			{decks && (
+			{!decks.length ? (
+				<View style={styles.noDecksContainer}>
+					<Text style={styles.noDecksText}>
+						👋 Not Decks added yet
+					</Text>
+				</View>
+			) : (
 				<FlatList
 					data={decks}
 					renderItem={renderItem}
@@ -36,5 +55,19 @@ const Decks = ({ navigation }) => {
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	loadingIndicator : { flex: 1, justifyContent: "center" },
+	noDecksContainer : {
+		flex           : 1,
+		justifyContent : "center",
+		alignItems     : "center",
+	},
+	noDecksText      : {
+		color      : "#000",
+		fontSize   : 25,
+		fontWeight : "bold",
+	},
+})
 
 export default Decks
